357b0c5f04a9e6a2a8b7ea69c5a64d5635defbe1
Geovanny251201 <69869435+Geovanny251201@users.noreply.github.com>
2023-05-17 22:03:07 -0600

    intento arreglo serverPdf

--- src/components/ButtonPdf.js
+++ src/components/ButtonPdf.js
@@ -6,7 +6,7 @@ export const ButtonPdf = () => {
  
 
   const downloadFile = () => {
-    window.open('http://localhost:4000/download?file=cv.pdf', '_blank');
+    window.open('https://geovanny-portafolio.netlify.app/download?file=cv.pdf', '_blank');
   }
 
   return (
