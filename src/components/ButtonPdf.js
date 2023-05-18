e5cbe41e6c3274f9de5af748145be4261be5e2cc
Geovanny251201 <69869435+Geovanny251201@users.noreply.github.com>
2023-05-18 10:10:50 -0600

    prueba diferente

--- src/components/ButtonPdf.js
+++ src/components/ButtonPdf.js
@@ -1,20 +1,20 @@
 import React from 'react';
-
+import CV from '../pdfs/cv-geo.pdf'
 
 
 export const ButtonPdf = () => {
 
  
 
-  const downloadFile = () => {
-    window.open('http://geovanny-portafolio.netlify.app/download?file=cv.pdf', '_blank');
-  }
+  // const downloadFile = () => {
+  //   window.open('http://geovanny-portafolio.netlify.app/download?file=cv.pdf', '_blank');
+  // }
 
   return (
     
- <button className='buttonPdf text-white ' onClick={downloadFile}>
-      <span> Descargar CV</span>
-  </button>   
+ 
+     <a href={CV} download='CV-Geo' className='btn'> Download CV</a>
+  
     
     
   );
